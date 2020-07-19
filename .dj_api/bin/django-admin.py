c8be9e377b0552e5dfdb9fa511e00a1a2bab9f80
mitchiban <mks135610@gmail.com>
2020-07-19 20:40:16 +1000

    start of django api project (udemy)

---
+++ .dj_api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mubuntu/Learning_tools/U_Build_API_with_django_REST_Framework/.dj_api/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
